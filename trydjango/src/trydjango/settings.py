99cfc11698cb3f2e52fa3c6d0f2ac6d03e62c6be
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-10-14 11:38:40 -0400

    Applying db migration

--- trydjango/src/trydjango/settings.py
+++ trydjango/src/trydjango/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -76,7 +77,7 @@ WSGI_APPLICATION = "trydjango.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"), 
     }
 }
 
